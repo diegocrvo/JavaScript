1276bf9c66e349a6d1c8beaf02f9a4366e6659c8
Diêgo Carvalho <157425806+diegocrvo@users.noreply.github.com>
2024-03-25 22:17:53 -0300

    Update Pomodoro Project

--- JavaScript_Projetos/Pomodoro_JavaScript/main.js
+++ JavaScript_Projetos/Pomodoro_JavaScript/main.js
@@ -4,13 +4,16 @@ var reset = document.getElementById('reset')
 var wm = document.getElementById('w_minutes')
 var ws = document.getElementById('w_seconds')
 
+var body = document.body
+var btn = document.getElementsByClassName('btn')
+
 var btnOptions = document.getElementsByClassName('btnOptions')
-btnOptions[0].style.background = '#744ab8'
+btnOptions[0].style.background = '#5a189a'
 
 var modo = 'work'
 
 btnOptions[0].addEventListener('click', function(){
-    btnOptions[0].style.background = '#744ab8'
+    btnOptions[0].style.background = '#5a189a'
     btnOptions[1].style.background = 'transparent'
     wm.innerText = 30
     ws.innerText = "00"
@@ -18,10 +21,13 @@ btnOptions[0].addEventListener('click', function(){
     start.innerText = 'START'
     stopInterval()
     startTimer = undefined
+    body.style.background = '#5a189a'
+    btn[0].style.color = '#5a189a'
+    btn[1].style.color = '#5a189a'
 })
 
 btnOptions[1].addEventListener('click', function(){
-    btnOptions[1].style.background = '#744ab8'
+    btnOptions[1].style.background = '#ff8500'
     btnOptions[0].style.background = 'transparent'
     wm.innerText = "05"
     ws.innerText = "00"
@@ -29,6 +35,9 @@ btnOptions[1].addEventListener('click', function(){
     start.innerText = 'START'
     stopInterval()
     startTimer = undefined
+    body.style.background = '#ff8500'
+    btn[0].style.color = '#ff8500'
+    btn[1].style.color = '#ff8500'
 })
 
 //store a reference to a timer variable
@@ -38,10 +47,12 @@ start.addEventListener('click', function(){
     if(startTimer === undefined){
         startTimer = setInterval(timer, 1000)
         start.innerText = 'PAUSE'
+        start.classList.add('btnCheked')
     } else {
         stopInterval()
         startTimer = undefined
         start.innerText = 'START'
+        start.className = 'btn'
     }
 })
 
